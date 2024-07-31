e18fbe527fee121ce828dadce4ea41b3306cdac5
hey1mryan <ryancai7519@gmail.com>
2024-07-31 18:48:13 -0400

    commit 2

--- script.js
+++ script.js
@@ -1,9 +1,34 @@
 let titleInput = document.getElementById("title_input");
 let mainTitle = document.getElementById("main_title");
+let submit = document.getElementById("submit_button");
 
-titleInput.addEventListener("input", 
-    function() {
-        mainTitle.textContent = titleInput.value;
+
+titleInput.addEventListener("input", function() {
+  mainTitle.textContent = titleInput.value;
+});
+
+function inputMissing(event) {
+  event.preventDefault();
+
+  // Get all input fields
+  let inputs = document.querySelectorAll(".adlib input");
+  let allFilled = true;
+
+  // Check if all input fields are filled
+  inputs.forEach(input => {
+    if (input.value.trim() === "") {
+      allFilled = false;
     }
-);
+  });
 
+  // Show alert if any field is empty
+  if (allFilled == false) {
+    alert("Please fill in every field!");
+  } else {
+    // Clear the page if all fields are filled
+    document.body.innerHTML = "";
+
+  }
+}
+
+submit.addEventListener("click", inputMissing);
